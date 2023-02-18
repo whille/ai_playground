226c01a473ee58f2e317a88976de1a3184047e13
whille <whille@163.com>
2023-02-18 12:55:21 +0800

    update

--- test_w2v.py
+++ test_w2v.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 from w2v import Word2Vec
+import sys
+sys.path.append('../poem')
+from t_poem import gen_sentence
 
 
-def test_word2vec():
+def test_simple():
     corpus = [
         ["king", "queen", "man", "woman"],
         ["dog", "cat", "fish", "bird", "snake"],
@@ -10,12 +13,20 @@ def test_word2vec():
         ["car", "bike", "boat", "plane"],
         ["apple", "orange", "banana", "grape"],
     ]
-    query_word = 'king'
-    # corpus = [["cat", "say", "meow"], ["dog", "say", "woof"]]
-    # query_word = "dog"
-    model = Word2Vec(corpus, window=2)
-    model.train(epochs=50)
+    model = Word2Vec(corpus, window=2, min_count=1)
+    model.train(epochs=500)
+    for query_word in ('man', 'car', 'grape', 'dog'):
+        similar_words = model.get_similar_words(query_word, k=3)
+        print(f"Most similar words to '%s': {similar_words}" % query_word)
 
-    # Test most similar words
-    similar_words = model.get_similar_words(query_word, k=3)
-    print(f"Most similar words to '%s': {similar_words}" % query_word)
+
+def test_poem():
+    fname = '../poem/data/全唐诗.txt'
+    corpus = []
+    for s in gen_sentence(fname):
+        corpus.append(list(s))
+    model = Word2Vec(corpus, window=3, min_count=5)
+    model.train(epochs=1)
+    for query_word in (list(s)):
+        similar_words = model.get_similar_words(query_word, k=3)
+        print(f"Most similar words to '%s': {similar_words}" % query_word)
--- w2v.py
+++ w2v.py
@@ -2,44 +2,49 @@
 import numpy as np
 from collections import defaultdict
 
+# https://nathanrooy.github.io/posts/2018-03-22/word2vec-from-scratch-with-python-and-numpy/
 
 class Word2Vec:
-    def __init__(self, sentences, embedding_size=10, window=5, negative_samples=5, learning_rate=0.025):
+    def __init__(self, sentences, window=5, min_count=5, negative_samples=5, learning_rate=0.025):
         self.sentences = sentences
-        self.embedding_size = embedding_size
         self.window_size = window
         self.negative_samples = negative_samples
         self.learning_rate = learning_rate
+        self.min_count = min_count
         self.init()
 
     def init(self):
         self.vocab = self.build_vocab()
-        print(f"vocab size: {len(self.vocab)}")
+        self.embedding_size = int(len(self.vocab) ** 0.5)
+        print(f"vocab size: {len(self.vocab)}, embedding_size: {self.embedding_size}")
         self.word2idx, self.idx2word = self.build_index()
-        # word_vectors
-        self.W1 = np.random.uniform(-0.8, 0.8, (len(self.vocab), self.embedding_size))
-        self.W2 = np.random.uniform(-0.8, 0.8, (self.embedding_size, len(self.vocab)))
 
     def train(self, epochs):
+        self.W1 = np.random.uniform(-0.8, 0.8, (len(self.vocab), self.embedding_size))
+        self.W2 = np.random.uniform(-0.8, 0.8, (self.embedding_size, len(self.vocab)))
         for epoch in range(epochs):
             self.loss = 0
             for sentence in self.sentences:
                 for i, w in enumerate(sentence):
+                    if w not in self.word2idx:
+                        continue
                     w_idx = self.word2idx[w]
                     context = sentence[max(0, i-self.window_size):i] + sentence[i+1:min(len(sentence), i+self.window_size+1)]
-                    for c in context:
-                        c_idx = self.word2idx[c]
-                        neg_samples = self.get_negative_samples(w_idx, c_idx)
-                        self.update_weights(w_idx, c_idx, neg_samples)
+                    c_idxs = [self.word2idx[c] for c in context if c in self.word2idx]
+                    self.update_weights(w_idx, c_idxs)
             if epoch % 10 == 0:
                 print(f"Epoch {epoch} Loss: {self.loss}")
 
     def build_vocab(self):
         word_freq = defaultdict(int)
+        print(f"len(sentences): {len(self.sentences)}")
         for sentence in self.sentences:
             for w in sentence:
                 word_freq[w] += 1
-        vocab = set(word_freq.keys())
+        vocab = set()
+        for w, c in word_freq.items():
+            if c >= self.min_count:
+                vocab.add(w)
         return vocab
 
     def build_index(self):
@@ -50,43 +55,44 @@ class Word2Vec:
             idx2word[i] = w
         return word2idx, idx2word
 
-    def get_negative_samples(self, w_idx, c_idx):
+    def negtive_sampling(self, w_idx, c_idxs):
         neg_samples = []
         while len(neg_samples) < self.negative_samples:
-            sample = np.random.choice(list(self.vocab))
-            if sample != self.idx2word[c_idx]:
-                neg_samples.append(self.word2idx[sample])
+            i = np.random.choice(len(self.vocab))
+            if i not in c_idxs:
+                neg_samples.append(i)
         return neg_samples
-
-    def forward(self, x):
-        h = self.W1[x]
-        u = np.dot(self.W2.T, h)
-        y_pred = self.softmax(u)
-        return y_pred
-
-    def backward(self, w_idx, c_idx, neg_samples, y):
-        d2 = np.copy(y)
-        d2[c_idx] -= 1
-        for n in neg_samples:
-            d2[n] -= 1
-        d1 = np.dot(self.W2, d2)
-        self.W2 -= self.learning_rate * np.outer(self.W1[w_idx], d2)
-        self.W1[w_idx] -= self.learning_rate * d1
 
     def softmax(self, x):
         e_x = np.exp(x - np.max(x))
         return e_x / e_x.sum(axis=0)
 
-    def update_weights(self, w_idx, c_idx, neg_samples):
-        y = self.forward(w_idx)
-        self.loss -= np.log(y[c_idx])
-        self.backward(w_idx, c_idx, neg_samples, y)
+    def forward(self, x):
+        # Since x is a one-hot encoded vector, ℎ is simply the k_th row of matrix W1
+        h = self.W1[x]
+        u = np.dot(self.W2.T, h)
+        y_pred = self.softmax(u)
+        return y_pred, h
 
-    def get_similar_words(self, word, k=10):
+    def backward(self, w_idx, c_idxs, neg_idxs, y_pred, h):
+        e = np.copy(y_pred)
+        e[c_idxs] -= 1
+        self.loss = e[c_idxs].sum()
+        e[neg_idxs] += 1
+        d2 = np.outer(h, e)
+        d1 = np.dot(self.W2, e.T)
+        self.W2 -= self.learning_rate * d2
+        self.W1[w_idx] -= self.learning_rate * d1
+
+    def update_weights(self, w_idx, c_idxs):
+        neg_idxs = self.negtive_sampling(w_idx, c_idxs)
+        y_pred, h = self.forward(w_idx)
+        self.backward(w_idx, c_idxs, neg_idxs, y_pred, h)
+
+    def get_similar_words(self, word, k=5):
         if word not in self.vocab:
             return None
-        word_idx = self.word2idx[word]
-        word_vec = self.W1[word_idx]
-        similarities = np.dot(self.W1, word_vec)
-        top_k = similarities.argsort()[::-1][:k]
+        w_idx = self.word2idx[word]
+        y_pred, h = self.forward(w_idx)
+        top_k = y_pred.argsort()[::-1][:k]
         return [self.idx2word[idx] for idx in top_k]
